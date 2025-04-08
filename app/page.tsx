86d8a6c0d66ff270c56c2a1fa1d1cf5af3057861
eminpromax <eminpromax@icloud.com>
2025-04-08 00:30:27 +0000

    color

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,14 @@
-"use client"
+
 import { Button } from "@/components/ui/button"
-import { Calendar } from "@/components/ui/calendar"
 import React from "react"
 
 
 export default function Home() {
 
-const [date, setDate] = React.useState<Date | undefined>(new Date()) 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Calendar
-          mode="single"
-          selected={date}
-          onSelect={setDate}
-          className="rounded-md border"
-        />
+     
         <Button variant="outline">Button</Button>
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
