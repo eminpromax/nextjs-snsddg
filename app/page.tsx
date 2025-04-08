1f6f1edc85a4a67c8b3ec2c0f6d4012d36db1442
eminpromax <eminpromax@icloud.com>
2025-04-08 00:28:22 +0000

    calendar

--- app/page.tsx
+++ app/page.tsx
@@ -10,19 +10,13 @@ const [date, setDate] = React.useState<Date | undefined>(new Date())
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-      
-
-
         <Calendar
           mode="single"
           selected={date}
           onSelect={setDate}
           className="rounded-md border"
         />
-
-
         <Button variant="outline">Button</Button>
-
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
 
